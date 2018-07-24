ad221f02aaf77926807fa402c0525f7dbaf36f17
Wisaroot Saowaros <33708390+wisarootsaowaros@users.noreply.github.com>
2018-07-25 02:06:56 +0700

    detail-list

--- lesser/buy-farmer-list.php
+++ lesser/buy-farmer-list.php
@@ -190,11 +190,11 @@
                     <?php while($row=mysqli_fetch_array($query,MYSQLI_ASSOC)){ ?>
                         <div class="col-md-4 text-center">
                             <div class="blog-inner">
-                                <a href="detail-product.php?id=<?php echo $row["id"];?>"><img class="img-responsive" src="myfile/<?php echo $row["picture"];?>" alt="Blog"></a>
+                                <a href="detail-farmer-list.php?id=<?php echo $row["id"];?>"><img class="img-responsive" src="myfile/<?php echo $row["picture"];?>" alt="Blog"></a>
                                 <div class="desc">
-                                    <h3><a href="detail-product.php?id=<?php echo $row["id"];?>"><?php echo $row["name"];?></a></h3>
+                                    <h3><a href="detail-farmer-list.php?id=<?php echo $row["id"];?>"><?php echo $row["name"];?></a></h3>
                                     <p><?php echo $row["detail"];?></p>
-                                    <p><a href="detail-product.php?id=<?php echo $row["id"];?>" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
+                                    <p><a href="detail-farmer-list.php?id=<?php echo $row["id"];?>" class="btn btn-primary btn-outline with-arrow">Read More<i class="icon-arrow-right"></i></a></p>
                                 </div>
                             </div>
                         </div>
